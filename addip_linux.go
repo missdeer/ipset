c61a7e9c9ba3c53f2dd85283582fd6d0356924a7
Fan Yang <missdeer@dfordsoft.com>
2020-04-18 09:45:05 +0800

    (*)support IPv6 #1

--- addip_linux.go
+++ addip_linux.go
@@ -3,10 +3,12 @@
 package ipset
 
 import (
+	"errors"
+	"net"
+
 	goipset "github.com/digineo/go-ipset/v2"
 	"github.com/mdlayher/netlink"
 	"github.com/ti-mo/netfilter"
-	"net"
 )
 
 var (
@@ -14,11 +16,25 @@ var (
 )
 
 func initLib() (err error) {
-	c, err = goipset.Dial(netfilter.ProtoIPv4, &netlink.Config{})
+	c, err = goipset.Dial(netfilter.ProtoUnspec, &netlink.Config{})
 	return
 }
 
 func addIP(ip net.IP, list string) error {
+	p, err := c.Header(list)
+	if err != nil {
+		log.Error("ipsetAddIP(): cannot get ipset %q header: %v", list, err)
+		return err
+	}
+	var typeMatch bool
+	if uint(p.Family.Value) == uint(netfilter.ProtoIPv4) {
+		typeMatch = (ip.To4() != nil)
+	} else if uint(p.Family.Value) == uint(netfilter.ProtoIPv6) {
+		typeMatch = (ip.To16() != nil)
+	}
+	if !typeMatch {
+		return errors.New("Not matched type")
+	}
 	AddIPCount.WithLabelValues(list).Add(1)
 	return c.Add(list, goipset.NewEntry(goipset.EntryIP(ip)))
 }
