3b23c940694e781d6e8c6381341a4f81bd47002d
Fan Yang <me@minidump.info>
2020-09-22 10:29:54 +0800

    Update metrics.go

--- metrics.go
+++ metrics.go
@@ -4,11 +4,12 @@ import (
 	"github.com/coredns/coredns/plugin"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 // Variables declared for monitoring.
 var (
-	AddIPCount = prometheus.NewCounterVec(prometheus.CounterOpts{
+	AddIPCount = promauto.NewCounterVec(prometheus.CounterOpts{
 		Namespace: plugin.Namespace,
 		Subsystem: "ipset",
 		Name:      "add_ip_count_total",
